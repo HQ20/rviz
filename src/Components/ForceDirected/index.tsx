2159ea7de0abea8461d3df5dab432df5edc05fe3
Bernardo Vieira <bernardo5@live.com.pt>
2019-07-25 10:51:52 +0100

    split methods in forcedirected

--- src/Components/ForceDirected/index.tsx
+++ src/Components/ForceDirected/index.tsx
@@ -3,20 +3,20 @@ import React from 'react';
 import './index.css';
 
 
-interface d3Node {
+interface ID3Node {
     id: string;
     group: number;
 }
 
-interface d3Link {
+interface ID3Link {
     source: string;
     target: string;
     value: number;
 }
 
-interface Graph {
-    nodes: d3Node[];
-    links: d3Link[];
+interface IGraph {
+    nodes: ID3Node[];
+    links: ID3Link[];
 }
 
 
@@ -24,17 +24,20 @@ interface Graph {
 interface IProps {
     width: number;
     height: number;
-    graph: Graph;
+    graph: IGraph;
 }
 
 export default class ForceDirected extends React.Component<IProps, {}> {
     private ref!: SVGSVGElement;
+    private color = d3.scaleOrdinal(d3.schemeCategory10);
+    private simulation: any;
+    private link: any;
+    private node: any;
 
     public componentDidMount() {
         const context: any = d3.select(this.ref);
-        const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-        const simulation: any = d3.forceSimulation()
+        this.simulation = d3.forceSimulation()
             .force('link', d3.forceLink()
                 .id((nodeData: d3.SimulationNodeDatum, i: number, nodesData: d3.SimulationNodeDatum[]) => {
                     // for some reason, this type is not correct
@@ -43,81 +46,81 @@ export default class ForceDirected extends React.Component<IProps, {}> {
             .force('charge', d3.forceManyBody())
             .force('center', d3.forceCenter(this.props.width / 2, this.props.height / 2));
 
-        const link = context.append('g')
+        this.link = context.append('g')
             .attr('class', 'links')
             .selectAll('line')
             .data(this.props.graph.links)
             .enter().append('line')
-            .attr('stroke-width', (d: d3Link) => {
+            .attr('stroke-width', (d: ID3Link) => {
                 return Math.sqrt(d.value);
             });
 
-        const node = context.append('g')
+        this.node = context.append('g')
             .attr('class', 'nodes')
             .selectAll('circle')
             .data(this.props.graph.nodes)
             .enter().append('circle')
             .attr('r', 5)
-            .attr('fill', (d: d3Node) => {
-                return color(d.group.toString());
+            .attr('fill', (d: ID3Node) => {
+                return this.color(d.group.toString());
             })
             .call(d3.drag()
-                .on('start', dragstarted)
-                .on('drag', dragged)
-                .on('end', dragended));
+                .on('start', this.dragstarted)
+                .on('drag', this.dragged)
+                .on('end', this.dragended));
 
-        node.append('title')
-            .text((d: d3Node) => {
+        this.node.append('title')
+            .text((d: ID3Node) => {
                 return d.id;
             });
 
-        simulation.nodes(this.props.graph.nodes).on('tick', ticked);
-        simulation.force('link').links(this.props.graph.links);
+        this.simulation.nodes(this.props.graph.nodes).on('tick', this.ticked);
+        this.simulation.force('link').links(this.props.graph.links);
+    }
 
-        function dragstarted(d: any) {
-            if (!d3.event.active) {
-                simulation.alphaTarget(0.3).restart();
-            }
-            d.fx = d.x;
-            d.fy = d.y;
+    public dragstarted = (d: any) => {
+        if (!d3.event.active) {
+            this.simulation.alphaTarget(0.3).restart();
         }
+        d.fx = d.x;
+        d.fy = d.y;
+    }
 
-        function dragged(d: any) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
+    public dragged = (d: any) => {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+
+    public dragended = (d: any) => {
+        if (!d3.event.active) {
+            this.simulation.alphaTarget(0);
         }
+        d.fx = null;
+        d.fy = null;
+    }
 
-        function dragended(d: any) {
-            if (!d3.event.active) {
-                simulation.alphaTarget(0);
-            }
-            d.fx = null;
-            d.fy = null;
-        }
+    public ticked = () => {
+        this.link
+            .attr('x1', (d: any) => {
+                return d.source.x;
+            })
+            .attr('y1', (d: any) => {
+                return d.source.y;
+            })
+            .attr('x2', (d: any) => {
+                return d.target.x;
+            })
+            .attr('y2', (d: any) => {
+                return d.target.y;
+            });
 
-        function ticked() {
-            link
-                .attr('x1', (d: any) => {
-                    return d.source.x;
-                })
-                .attr('y1', (d: any) => {
-                    return d.source.y;
-                })
-                .attr('x2', (d: any) => {
-                    return d.target.x;
-                })
-                .attr('y2', (d: any) => {
-                    return d.target.y;
-                });
-
-            node
-                .attr('cx', (d: any) => {
-                    return d.x;
-                })
-                .attr('cy', (d: any) => {
-                    return d.y;
-                });
-        }
+        this.node
+            .attr('cx', (d: any) => {
+                return d.x;
+            })
+            .attr('cy', (d: any) => {
+                return d.y;
+            });
     }
 
     public render() {
