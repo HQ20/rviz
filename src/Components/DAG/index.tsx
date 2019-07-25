6523e4a567e388ad6fd1215c07d05b4e82a6b6cc
Bernardo Vieira <bernardo5@live.com.pt>
2019-07-25 10:57:41 +0100

    fix lint warnings DAG

--- src/Components/DAG/index.tsx
+++ src/Components/DAG/index.tsx
@@ -1,20 +1,8 @@
-import React from 'react';
 import * as d3 from 'd3';
 import * as d3Dag from 'd3-dag';
+import React from 'react';
 
 
-
-type d3Node = {
-    children: [],
-    data: {
-        id: string
-        parentIds: [string]
-    },
-    id: string,
-    layer: number,
-    x: number,
-    y: number,
-}
 
 interface IProps {
     width: number;
@@ -25,40 +13,40 @@ interface IProps {
 export default class DAG extends React.Component<IProps, {}> {
     private ref!: SVGSVGElement;
 
-    componentDidMount() {
+    public componentDidMount() {
         const { width, height, grafo } = this.props;
         const context: any = d3.select(this.ref);
 
         // This code only handles rendering
         const nodeRadius = 20;
-        var svgNode = context
-            .attr("width", width + nodeRadius * 2)
-            .attr("height", height + nodeRadius * 2)
-            .attr("viewbox", `${-nodeRadius} ${-nodeRadius} ${width + 2 * nodeRadius} ${height + 2 * nodeRadius}`);
+        const svgNode = context
+            .attr('width', width + nodeRadius * 2)
+            .attr('height', height + nodeRadius * 2)
+            .attr('viewbox', `${-nodeRadius} ${-nodeRadius} ${width + 2 * nodeRadius} ${height + 2 * nodeRadius}`);
 
         const defs = svgNode.append('defs'); // For gradients
 
         const layerings = {
-            "Simplex (slow)": d3Dag.layeringSimplex(),
-            "Longest Path (fast)": d3Dag.layeringLongestPath(),
-            "Coffman Graham (medium)": d3Dag.layeringCoffmanGraham(),
+            'Coffman Graham (medium)': d3Dag.layeringCoffmanGraham(),
+            'Longest Path (fast)': d3Dag.layeringLongestPath(),
+            'Simplex (slow)': d3Dag.layeringSimplex(),
         };
         const decrossings = {
-            "Optimal (slow)": d3Dag.decrossOpt(),
-            "Two Layer Opt (medium)": d3Dag.decrossTwoLayer().order(d3Dag.twolayerOpt()),
-            "Two Layer (flast)": d3Dag.decrossTwoLayer(),
+            'Optimal (slow)': d3Dag.decrossOpt(),
+            'Two Layer (flast)': d3Dag.decrossTwoLayer(),
+            'Two Layer Opt (medium)': d3Dag.decrossTwoLayer().order(d3Dag.twolayerOpt()),
         };
         const coords = {
-            "Vertical (slow)": d3Dag.coordVert(),
-            "Minimum Curves (slow)": d3Dag.coordMinCurve(),
-            "Greedy (medium)": d3Dag.coordGreedy(),
-            "Center (fast)": d3Dag.coordCenter(),
+            'Center (fast)': d3Dag.coordCenter(),
+            'Greedy (medium)': d3Dag.coordGreedy(),
+            'Minimum Curves (slow)': d3Dag.coordMinCurve(),
+            'Vertical (slow)': d3Dag.coordVert(),
         };
-        const layering = "Simplex (slow)";
-        const decross = "Optimal (slow)";
-        const coord = "Vertical (slow)";
+        const layering = 'Simplex (slow)';
+        const decross = 'Optimal (slow)';
+        const coord = 'Vertical (slow)';
 
-        var dag = d3Dag.dagStratify()(grafo);
+        const dag = d3Dag.dagStratify()(grafo);
         const layout = d3Dag.sugiyama()
             .size([width, height])
             .layering(layerings[layering])
@@ -69,7 +57,7 @@ export default class DAG extends React.Component<IProps, {}> {
 
         const steps = dag.size();
         const interp = d3.interpolateRainbow;
-        var colorMap: [string] = [] as any;
+        const colorMap: [string] = [] as any;
         dag.each((node: { id: number }, i: any) => {
             colorMap[node.id] = interp(i / steps);
         });
@@ -77,8 +65,8 @@ export default class DAG extends React.Component<IProps, {}> {
         // How to draw edges
         const line = d3.line()
             .curve(d3.curveCatmullRom)
-            .x(function (d: any) { return d.x + nodeRadius })
-            .y(function (d: any) { return d.y + nodeRadius });
+            .x((d: any) => d.x + nodeRadius)
+            .y((d: any) => d.y + nodeRadius);
 
         // Plot edges
         svgNode.append('g')
@@ -86,9 +74,7 @@ export default class DAG extends React.Component<IProps, {}> {
             .data(dag.links())
             .enter()
             .append('path')
-            .attr('d', function (d: any) {
-                return line(d.data.points)
-            })
+            .attr('d', (d: any) => line(d.data.points))
             .attr('fill', 'none')
             .attr('stroke-width', 3)
             .attr('stroke', (stroke: any) => {
@@ -111,16 +97,13 @@ export default class DAG extends React.Component<IProps, {}> {
             .data(dag.descendants())
             .enter()
             .append('g')
-            .attr('transform', function (transform: any) {
-                return `translate(${transform.x + nodeRadius}, ${transform.y + nodeRadius})`
-            });
+            .attr('transform', (transform: any) =>
+                `translate(${transform.x + nodeRadius}, ${transform.y + nodeRadius})`);
 
         // Plot node circles
         nodes.append('circle')
             .attr('r', nodeRadius)
-            .attr("fill", function (n: any) {
-                return colorMap[n.id];
-            })
+            .attr('fill', (n: any) => colorMap[n.id]);
 
         const arrow = d3.symbol().type(d3.symbolTriangle).size(nodeRadius * nodeRadius / 5.0);
         svgNode.append('g')
@@ -129,17 +112,21 @@ export default class DAG extends React.Component<IProps, {}> {
             .enter()
             .append('path')
             .attr('d', arrow)
-            .attr('transform', function (transform: any) {
+            .attr('transform', (transform: any) => {
                 const [end, start] = transform.data.points.reverse();
-                // This sets the arrows the node radius (20) + a little bit (3) away from the node center, on the last line segment of the edge. This means that edges that only span ine level will work perfectly, but if the edge bends, this will be a little off.
+                // This sets the arrows the node radius (20) + a little bit (3) away from the
+                // node center, on the last line segment of the edge. This means that edges
+                // that only span ine level will work perfectly, but if the edge bends,
+                // this will be a little off.
                 const dx = start.x - end.x;
                 const dy = start.y - end.y;
                 const scale = nodeRadius * 1.15 / Math.sqrt(dx * dx + dy * dy);
                 // This is the angle of the last line segment
                 const angle = Math.atan2(-dy, -dx) * 180 / Math.PI + 90;
-                return `translate(${end.x + nodeRadius + dx * scale}, ${end.y + nodeRadius + dy * scale}) rotate(${angle})`;
+                return `translate(${end.x + nodeRadius + dx * scale},`
+                    + `${end.y + nodeRadius + dy * scale}) rotate(${angle})`;
             })
-            .attr('fill', function (fill: any) { return colorMap[fill.target.id] })
+            .attr('fill', (fill: any) => colorMap[fill.target.id])
             .attr('stroke', 'white')
             .attr('stroke-width', 1.5);
 
@@ -153,13 +140,16 @@ export default class DAG extends React.Component<IProps, {}> {
             .attr('fill', 'white');
     }
 
-    render() {
+    public render() {
         const { width, height } = this.props;
 
         return (
-            <svg className="container" ref={(ref: SVGSVGElement) => this.ref = ref}
-                width={width} height={height}>
-            </svg>
+            <svg
+                className="container"
+                ref={(ref: SVGSVGElement) => this.ref = ref}
+                width={width}
+                height={height}
+            />
         );
     }
 }
