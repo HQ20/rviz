be86f5290e2d6e0b414808318ba02ebf1889bcbd
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-09 21:18:38 +0100

    create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import DAG from './Components/DAG';
+import ForceDirected from './Components/ForceDirected';
+import VoroniArcMap from './Components/VoroniArcMap';
+
+
+export { DAG, ForceDirected, VoroniArcMap };
