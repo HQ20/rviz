5efb4e73808573fe7a7613c5c133897261d6c497
Bernardo Vieira <bernardo5@live.com.pt>
2019-08-30 19:14:46 +0100

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
 
-import { VoroniArcMap } from 'reviz'
+import { VoroniArcMap, DAG, ForceDirected } from 'reviz'
+import dataDAG from './dummyData/DAG';
+import dataForceDirected from './dummyData/ForceDirected';
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <VoroniArcMap width={980} height={540} />
+                <DAG
+                    width={600}
+                    height={600}
+                    grafo={dataDAG}
+                    layering={'Simplex (slow)'}
+                    decross={'Optimal (slow)'}
+                    coord={'Vertical (slow)'}
+                    arrow={'end'} />
+
+                <ForceDirected
+                    width={600}
+                    height={600}
+                    graph={dataForceDirected}
+                />
             </div>
         )
     }
