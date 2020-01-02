d2ccca6eb2790c461f6291af0f3991448c0a8b26
Bernardo Vieira <bernardo5@live.com.pt>
2020-01-02 17:48:56 +0000

    stop tracking dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import DAG from './Components/DAG';
-import ForceDirected from './Components/ForceDirected';
-import VoroniArcMap from './Components/VoroniArcMap';
-export { DAG, ForceDirected, VoroniArcMap };
